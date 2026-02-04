6caba662baf4f45c85af1fcb448207e71f9e4a96
dookim <hadarytal@gmail.com>
2026-02-04 16:45:32 +0200

    changed input, input buffer, output and formatted code to better fit conventions.

--- boot.asm
+++ boot.asm
@@ -1,26 +1,45 @@
-mov ah, 0x0e
-mov al, 'A'
+[org 0x7c00]        ; Sets origin of memory to account for offset
+    mov bx, buffer
+    mov cl, 0
 
-loop:
+takeInput:
+    mov ah, 0
+    int 0x16
+    cmp al, 0x0d
+    je prnt
+    cmp cl, 10
+    je prnt
+    inc cl
+    mov ah, 0x0e
     int 0x10
-    cmp al, 0x61
-    jge upper
-    jmp lower
+    mov [bx], al
+    inc bx
+    jmp takeInput
 
-upper:
-    sub al, 0x1f
-    cmp al, 'Z'+1
-    je exit
-    jmp loop
+prnt:
+    mov [bx], 0
+    mov ah, 0x0e
+    mov al, 0x0d
+    int 0x10
+    mov al, 0x0a
+    int 0x10
+    mov bx, buffer      ; Pointer to buffer
 
-lower:
-    add al, 0x21
-    cmp al, 'z'+1
+printString:
+    mov al, [bx]
+    cmp al, 0
     je exit
-    jmp loop
+    int 0x10
+    inc bx
+    jmp printString
 
 exit:
     jmp $
+
+buffer:
+times 10 db 0
+
+
 
 times 510-($-$$) db 0
 db 0x55, 0xaa
